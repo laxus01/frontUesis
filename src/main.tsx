c06438ad13eac545469b85eeace04f1bfc301a0f
JOSE TUÑON <ut_josetv@hotmail.com>
2025-09-02 05:46:27 -0500

    Se agrega el icono de la aplicación

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
+import iconJT from './assets/icon_jt.png';
+
+// Ensure favicon is set and bundled by Vite
+const ensureFavicon = () => {
+  let link = document.querySelector("link[rel~='icon']") as HTMLLinkElement | null;
+  if (!link) {
+    link = document.createElement('link');
+    link.rel = 'icon';
+    document.head.appendChild(link);
+  }
+  link.type = 'image/png';
+  link.href = iconJT;
+};
+ensureFavicon();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
